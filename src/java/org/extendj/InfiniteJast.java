db08eb3bf7e08d797be54a8e4736420bbad17ffb
Erick Eduardo Ochoa Lopez <eochoa@ualberta.ca>
2016-11-04 19:41:45 -0600

    initial commit

---
+++ src/java/org/extendj/InfiniteJast.java
@@ -0,0 +1,25 @@
+package org.extendj;
+
+import org.extendj.ast.CompilationUnit;
+
+public class InfiniteJast extends JavaChecker {
+
+  public static void main(String args[]) {
+    int exitCode = new InfiniteJast().run(args);
+    if (exitCode != 0) {
+      System.exit(exitCode);
+    }
+  }
+
+  @Override
+  protected int processCompilationUnit(CompilationUnit unit) throws Error {
+    // Replace the following super call to skip semantic error checking in unit.
+    return super.processCompilationUnit(unit);
+  }
+
+  /** Called by processCompilationUnit when there are no errors in the argument unit.  */
+  @Override
+  protected void processNoErrors(CompilationUnit unit) {
+    unit.process();
+  }
+}
